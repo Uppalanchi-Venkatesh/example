eea6f4c6a41bc1c5a8370d928baf407841cff3a8
Uppalanchi-Venkatesh <uppalanchivenkatesh@gmail.com>
2020-08-07 12:59:59 +0530

    Just changed

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ app.get('/', function(req, res){
     res.sendFile(__dirname+"/FrontEnd/HTML/index.html");
 })
 
-app.get('/', function(req, res){
+app.get('/second', function(req, res){
     res.sendFile(__dirname+"/FrontEnd/HTML/second.html");
 })
 
-var port= process.env.PORT  || 4225;
+var port= process.env.PORT  || 4226;
 
 app.listen(port, function(){
     console.log("Site Running on http://localhost:"+port);
