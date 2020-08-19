d93f02131a444a82065663866fa446a7984abe49
Uppalanchi-Venkatesh <uppalanchivenkatesh@gmail.com>
2020-08-19 08:19:58 +0530

    Tambola game

---
+++ FrontEnd/JS/tambola.js
@@ -0,0 +1,43 @@
+var arr=[];
+var m,v=0;
+
+function myFunction(){
+    for(m=0;m<90;m++)
+        arr[m]=m+1;
+    var i, j, temp;
+    for (i = arr.length -1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i+1));
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+function generateNum(){
+    if(v==0)
+        myFunction();
+    if(v!=90){
+        document.getElementById("rcorners1").innerHTML=arr[v];
+        if(v>0)
+            document.getElementById("rcorners2").innerHTML=arr[v-1];
+        m="n"+arr[v];
+        document.getElementById(m).innerHTML=arr[v];
+        v++;
+    }
+    else{
+        window.alert("Game End");
+        window.alert("Restart Game Again");
+    }
+}
+
+function clearboard(){
+    document.getElementById("rcorners1").innerHTML="";
+    document.getElementById("rcorners2").innerHTML="";
+    for(let i=0;i<90;i++){
+        arr[i]=i+1;
+        m = "n" + arr[i];
+        document.getElementById(m).innerHTML="";
+    }
+    myFunction();
+    v=0;
+}
--- server.js
+++ server.js
@@ -8,11 +8,15 @@ app.get('/', function(req, res){
     res.sendFile(__dirname+"/FrontEnd/HTML/index.html");
 })
 
+app.get('/tambola', function(req, res){
+    res.sendFile(__dirname+"/FrontEnd/HTML/tambola.html");
+})
+
 app.get('/second', function(req, res){
     res.sendFile(__dirname+"/FrontEnd/HTML/second.html");
 })
 
-var port= process.env.PORT  || 4226;
+var port= process.env.PORT  || 5000;
 
 app.listen(port, function(){
     console.log("Site Running on http://localhost:"+port);
