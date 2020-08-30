3bc5c11832ad7efdf3b5bd1da9b164658d3a42bf
Uppalanchi-Venkatesh <uppalanchivenkatesh@gmail.com>
2020-08-30 16:51:18 +0530

    More specific-1

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
 var db=require("./Backend/DB/DBconnect");
 var userlib=require("./Backend/Lib/userlib");
 var RegisterUserLib = require('./Backend/Lib/RegisterUserLib');
 var LoginUserLib = require('./Backend/Lib/LoginUserLib');
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
 db.connect();
 app.use(express.static(__dirname+'/FrontEnd'));
 
